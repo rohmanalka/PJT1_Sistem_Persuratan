b0a56629288750891cab183c879f1939fd55b689
rohmanalka <rohmanalka06@gmail.com>
2026-01-15 11:40:12 +0700

    feat: setup API auth

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,10 +4,12 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\PejabatMiddleware;
+use Illuminate\Http\Middleware\HandleCors;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
@@ -16,6 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'pejabat' => PejabatMiddleware::class,
             'role' => \App\Http\Middleware\RoleMiddleware::class,
         ]);
+        $middleware->append(HandleCors::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
